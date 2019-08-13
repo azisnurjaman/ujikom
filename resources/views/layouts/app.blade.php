47a563c32d4d01a42c5b461e38ea4c1f63ba7a4b
Azis Nurjaman <azisnurjaman03@gmail.com>
2019-08-13 11:38:26 +0700

    db4free

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,12 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                @guest
+
+                @role('admin')
+                
+                @endrole
+                @guest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
