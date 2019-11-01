9583887cf6fa491a17b78577c682016d58cd0d5c
Azis Nurjaman <azisnurjaman03@gmail.com>
2019-11-01 14:34:05 +0700

    DONE

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="/login/images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="/log/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/login/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/login/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="/log/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/login/css/util.css">
-	<link rel="stylesheet" type="text/css" href="/login/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/log/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/log/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -42,21 +42,21 @@
 	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
-	<script src="/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="/log/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/vendor/animsition/js/animsition.min.js"></script>
+	<script src="/log/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/vendor/bootstrap/js/popper.js"></script>
-	<script src="/login/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/log/vendor/bootstrap/js/popper.js"></script>
+	<script src="/log/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/vendor/select2/select2.min.js"></script>
+	<script src="/log/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/vendor/daterangepicker/moment.min.js"></script>
-	<script src="/login/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="/log/vendor/daterangepicker/moment.min.js"></script>
+	<script src="/log/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/vendor/countdowntime/countdowntime.js"></script>
+	<script src="/log/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-	<script src="/login/js/main.js"></script>
+	<script src="/log/js/main.js"></script>
 
 </body>
 </html>
